0479e3da8e3e28f3c6759c24fcb285c9c8ab39dd
Piet <pietmaier.github@protonmail.com>
2022-06-21 12:06:42 +0200

    Update Player Sprites & Add Attack Animation

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,27 +4,29 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float speed = 1;
+    public float movementSpeed = 1;
     public int health;
     public float attackRange;
     public int damage;
     public int highscore;
     public string playerName;
+    
+    // Movement
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movementDirection;
+    
+    // Animations
+    private Animator _animator;
+    private readonly int _isMoving = Animator.StringToHash("isMoving");
+    private readonly int _attack = Animator.StringToHash("attack");
+    
+    private SpriteRenderer _renderer;
 
     public GameObject healtbar;
 
     private float distToTarget;
 
     public Transform target;
-
-    private Vector2 _direction;
-
-    private Rigidbody2D _rigidbody;
-    
-    private Animator _animator;
-    private readonly int _isMoving = Animator.StringToHash("isMoving");
-
-    private SpriteRenderer _renderer;
 
     // This method is called once at the start of the game.
     public void Start()
@@ -39,6 +41,13 @@ public class PlayerScript : MonoBehaviour
     // This method is called once per frame.
     public void Update()
     {
+        Move();
+        
+        if (Input.GetButton("Fire1"))
+        {
+            Attack();
+        }
+        
         HealthbarUpdate();
        
         if (health <= 0)
@@ -48,31 +57,41 @@ public class PlayerScript : MonoBehaviour
 
         if (target != null)
             distToTarget = Vector2.Distance(transform.position, target.position);
+    }
+    
+    // This method does not depend on the frame rate, so it should be used for calculations related to physics.
+    public void FixedUpdate()
+    {
+        // Delta Time = Time Since Last Function Call => Movement Not Affected By Function Interval
+        _rigidbody.MovePosition(_rigidbody.position + Time.fixedDeltaTime * movementSpeed * _movementDirection);
+    }
 
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Attack();
-        }
-
-        _direction.x = Input.GetAxisRaw("Horizontal");
-        _direction.y = Input.GetAxisRaw("Vertical");
+    // Get Movement Input & Handle Movement Animation
+    private void Move()
+    {
+        _movementDirection.x = Input.GetAxisRaw("Horizontal");
+        _movementDirection.y = Input.GetAxisRaw("Vertical");
 
         // Normalized => Equal Velocity In All Directions
-        _direction = _direction.normalized;
+        _movementDirection = _movementDirection.normalized;
         
         // Toggle Movement Animation
-        _animator.SetBool(_isMoving, _direction != Vector2.zero);
+        _animator.SetBool(_isMoving, _movementDirection != Vector2.zero);
 
         // Flip Sprite Depending On Direction
-        _renderer.flipX = _direction.x switch //switch case
+        _renderer.flipX = _movementDirection.x switch
         {
-            < 0 => true, //0
-            > 0 => false, //1
-            _ => _renderer.flipX //default
+            < 0 => true,
+            > 0 => false,
+            _ => _renderer.flipX
         };
     }
-    public void Attack()
+    
+    private void Attack()
     {
+        // Toggle Attack Animation
+        _animator.SetTrigger(_attack);
+        
         if (target != null & distToTarget < attackRange)
             target.GetComponent<NpcController>().Damage(damage);
     }
@@ -91,13 +110,6 @@ public class PlayerScript : MonoBehaviour
         {
             target = null;
         }
-    }
-
-    // This method does not depend on the frame rate, so it should be used for calculations related to physics.
-    public void FixedUpdate()
-    {
-        // Delta Time = Time Since Last Function Call => Movement Not Affected By Function Interval
-        _rigidbody.MovePosition(_rigidbody.position + Time.fixedDeltaTime * speed * _direction);
     }
 
     public void Damage(int dmg)
